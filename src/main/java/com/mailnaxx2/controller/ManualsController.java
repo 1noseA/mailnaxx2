80fb3e1bfc0ce309edda5c7ca63d05ac46fcd863
1noseA <1noseA2L@gmail.com>
2024-04-03 21:50:51 +0900

    編集画面初期表示

--- src/main/java/com/mailnaxx2/controller/ManualsController.java
+++ src/main/java/com/mailnaxx2/controller/ManualsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
+import com.mailnaxx2.constants.CommonConstants;
 import com.mailnaxx2.form.ManualsForm;
 import com.mailnaxx2.jackson.Manuals;
 import com.mailnaxx2.security.LoginUserDetails;
@@ -92,5 +93,38 @@ public class ManualsController {
         manualInfo = restTemplate.postForObject(POST_URL, manualsForm, Manuals.class);
 
         return "redirect:/manual/list";
+    }
+
+    // 編集画面初期表示
+    @PostMapping("/manual/edit")
+    public String edit(int manualId,
+                        @ModelAttribute ManualsForm manualsForm,
+                        Model model,
+                        @AuthenticationPrincipal LoginUserDetails loginUser) {
+        // 詳細情報を取得
+        manualInfo = restTemplate.getForObject(GET_URL + "/" + manualId, Manuals.class);
+        // 入力フォームに設定
+        setInputForm(manualInfo, manualsForm);
+
+        model.addAttribute("manualId", manualId);
+        model.addAttribute("loginUserInfo", loginUser.getLoginUser());
+        return "manual/create";
+    }
+
+    // 入力フォームに設定
+    private void setInputForm(Manuals manualInfo, ManualsForm manualsForm) {
+        manualsForm.setUserId(manualInfo.getUserId());
+        manualsForm.setDisplayOrder(manualInfo.getDisplayOrder());
+        manualsForm.setTitle(manualInfo.getTitle());
+        String[] startDate = manualInfo.getStartDate().toString().split(CommonConstants.HALF_HYPHEN);
+        manualsForm.setStartYear(startDate[0]);
+        manualsForm.setStartMonth(startDate[1].replaceFirst("^0+", ""));
+        manualsForm.setStartDay(startDate[2].replaceFirst("^0+", ""));
+        String[] endDate = manualInfo.getEndDate().toString().split(CommonConstants.HALF_HYPHEN);
+        manualsForm.setEndYear(endDate[0]);
+        manualsForm.setEndMonth(endDate[1].replaceFirst("^0+", ""));
+        manualsForm.setEndDay(endDate[2].replaceFirst("^0+", ""));
+        manualsForm.setContent(manualInfo.getContent());
+        manualsForm.setLink(manualInfo.getLink());
     }
 }
