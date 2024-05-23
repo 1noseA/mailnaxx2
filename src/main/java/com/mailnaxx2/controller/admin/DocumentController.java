c48b8b834f909f39a7aa9bef5ab1b04e0858333c
1noseA <1noseA2L@gmail.com>
2024-05-23 23:46:52 +0900

    アップロード画面作成

--- src/main/java/com/mailnaxx2/controller/admin/DocumentController.java
+++ src/main/java/com/mailnaxx2/controller/admin/DocumentController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mailnaxx2.security.LoginUserDetails;
 
@@ -12,6 +13,14 @@ public class DocumentController {
 
     @GetMapping("/admin/document/upload")
     public String index(Model model, @AuthenticationPrincipal LoginUserDetails loginUser) {
+        model.addAttribute("loginUserInfo", loginUser.getLoginUser());
+        return "admin/document/upload";
+    }
+
+    @PostMapping("/admin/document/upload")
+    public String upload(Model model, @AuthenticationPrincipal LoginUserDetails loginUser) {
+        // アップロード処理
+
         model.addAttribute("loginUserInfo", loginUser.getLoginUser());
         return "admin/document/upload";
     }
