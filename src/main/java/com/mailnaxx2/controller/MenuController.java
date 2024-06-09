82bc56afe5d10356af2b2705e530af93d5bc9d0d
1noseA <1noseA2L@gmail.com>
2024-06-09 23:16:17 +0900

    権限チェックを追加

--- src/main/java/com/mailnaxx2/controller/MenuController.java
+++ src/main/java/com/mailnaxx2/controller/MenuController.java
@@ -6,13 +6,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.mailnaxx2.security.LoginUserDetails;
+import com.mailnaxx2.values.RoleClass;
 
 @Controller
 public class MenuController {
 
     @GetMapping("/admin/menu")
     public String index(Model model, @AuthenticationPrincipal LoginUserDetails loginUser) {
-        model.addAttribute("loginUserInfo", loginUser.getLoginUser());
-        return "menu/menu";
+        // 総務以外は遷移できないようにする
+        if (loginUser.getLoginUser().getRoleClass().equals(RoleClass.AFFAIRS.getCode())) {
+            model.addAttribute("loginUserInfo", loginUser.getLoginUser());
+            return "menu/menu";
+        } else {
+            return "redirect:/top";
+        }
     }
 }
