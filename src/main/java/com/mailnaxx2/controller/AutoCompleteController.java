ab99f6505d9e5b47538af38637514cbc85658a03
1noseA <1noseA2L@gmail.com>
2024-06-16 23:46:56 +0900

    修正

--- src/main/java/com/mailnaxx2/controller/AutoCompleteController.java
+++ src/main/java/com/mailnaxx2/controller/AutoCompleteController.java
@@ -3,8 +3,6 @@ package com.mailnaxx2.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +18,15 @@ public class AutoCompleteController {
     @Autowired
     UsersService usersService;
 
-    /** 全商品名をオートコンプリートに渡す. */
     @GetMapping("/auto-complete")
-    public ResponseEntity<String> getAutoComplete(){
+    public String getAutoComplete(){
         try {
             List<String> nameList = usersService.findAllName();
-            // ListをJSON文字列に変換
-            String json = objectMapper.writeValueAsString(nameList);
-            return ResponseEntity.ok(json);
+            // ListをJSON文字列に変換して直接返す
+            return objectMapper.writeValueAsString(nameList);
         } catch (Exception e) {
-            // 例外処理
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            // 例外が発生した場合は、空のJSONオブジェクトを返す
+            return "{}";
         }
     }
 }
